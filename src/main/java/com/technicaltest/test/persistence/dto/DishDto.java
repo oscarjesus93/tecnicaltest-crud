88ac4627556704c12692f03b606b79e507bad8c3
oscar dev <oscar100493@gmail.com>
2023-03-24 07:58:34 -0400

    add changes for field input and output

--- src/main/java/com/technicaltest/test/persistence/dto/DishDto.java
+++ src/main/java/com/technicaltest/test/persistence/dto/DishDto.java
@@ -1,5 +1,6 @@
 package com.technicaltest.test.persistence.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,10 +8,19 @@ import java.time.LocalDate;
 @Data
 public class DishDto {
 
+    @JsonProperty("name")
     private String nombre;
+
+    @JsonProperty("color")
     private String color;
+
+    @JsonProperty("price")
     private double precio;
+
+    @JsonProperty("field")
     private String campos;
+
+    @JsonProperty("offer")
     private Integer oferta;
 
 }
--- src/main/java/com/technicaltest/test/persistence/entity/Dish.java
+++ src/main/java/com/technicaltest/test/persistence/entity/Dish.java
@@ -1,5 +1,6 @@
 package com.technicaltest.test.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -13,12 +14,26 @@ public class Dish {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonProperty("name")
     private String nombre;
+
+    @JsonProperty("date")
     private LocalDate fecha;
+
+    @JsonProperty("startActivity")
     private LocalDate inicioActividad;
+
+    @JsonProperty("color")
     private String color;
+
+    @JsonProperty("price")
     private double precio;
+
+    @JsonProperty("field")
     private String campos;
+
+    @JsonProperty("offer")
     private boolean oferta;
 
 }
